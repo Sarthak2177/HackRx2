123afac3e1c80522894bc143e665e65486f2e34c
Sarthak2177 <sarthak.22310427@viit.ac.in>
2025-08-08 15:56:41 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# Upgrade pip
 pip install --upgrade pip
-
-# Try to uninstall fitz (optional and safe)
 pip uninstall -y fitz || true
-
-# Install PyMuPDF correctly
-pip install --no-cache-dir PyMuPDF==1.22.5
+pip install -r requirements.txt
